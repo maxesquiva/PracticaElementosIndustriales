939eab6010f698ea612a9e0562f5c7d80218a7cc
Manuel Esquiva <manuelesquivacalvo96@gmail.com>
2022-12-09 23:51:26 +0100

    limpieza

--- Js/jsFormulario.js
+++ Js/jsFormulario.js
@@ -1,34 +1,9 @@
-// const formulario = [{
-//     // poner los elementos en minusculas 
-//     //     nombreElemento: "detector de humos",
-//     //     descripcion: "Los sensores de humo son capaces de detectar el humo de un lugar a tiempo",
-//     //     numeroSerie: 1582,
-//     //     estado: true,
-//     //     prioridad: "Alta"
-
-//     // }, {
-//     //     nombreElemento: "Medidor de presión",
-//     //     descripcion: "Los sensores medidores de presión son muy utilizados en el sector agrícola para conocer",
-//     //     numeroSerie: 1582,
-//     //     estado: true,
-//     //     prioridad: "Media"
-//     // }, {
-//     //     nombreElemento: "Control de la humedad",
-//     //     descripcion: "Permiten tener controlados factores como el clima o el almacenamiento de productos perecederos",
-//     //     numeroSerie: 1756,
-//     //     estado: false,
-//     //     prioridad: "Baja"
-// }
-// ];
-
-
-
 
 const URL = './ws/getElement.php';
 const URL_delete = './ws/deleteElement.php/?id=';
 const nombre_URL = './ws/getElement.php/data/nombre';
 const URL_Modify = './ws/modifyElements.php/?id=';
-// let maps = [];
+
 
 window.onload = () => {
     cargarElementos().then(elementos => cargaTexto(elementos));
@@ -110,12 +85,6 @@ function cargaTexto(jsonElementos) { //crear un boton por cada personaje de la a
             }).then((result) => {
                 if (result.isConfirmed) {
                     trBorrado(jsonElementos.data[index].id, this.parentElement);
-                    // this.parentElement.remove();
-                    //   Swal.fire(
-                    //     'Borrado!',
-                    //     'Tu elemento ha sido eliminado.',
-                    //     'success'
-                    //   )
                 }
             })
         };
@@ -125,20 +94,6 @@ function cargaTexto(jsonElementos) { //crear un boton por cada personaje de la a
         }
     });
 
-
-    
-    // editar.onclick = function (id){
-    //     fetch("modifyElements.php",{
-    //         method:"POST",
-    //         body: id
-    //     }).them(response => response.json()).them(response =>{
-    //         TdModNombre_.value = response.tdNombre;
-    //         idDescripcion.value = response.tdDescripcion;
-    //         tdNumSerie.value = response.tdNumSerie;
-    //         tdEstado.value = response.tdEstado;
-    //         tdPrioridad.value = response.tdPrioridad;
-    //     })
-    // }
 }
 
 
@@ -164,54 +119,6 @@ function trBorrado(id, fila) {
         });
 }
 
-
-
-
-// const cuerpoTabla = document.querySelector("#cuerpoTabla");
-// formulario.forEach(formulario => {
-
-//     const tr = document.createElement("tr");
-//     tr.setAttribute("id", "trCreado")//asi proporcionamos atributos a los elementos que se crean.
-
-//     let boton = document.createElement("button");
-//     boton.textContent = formulario.boton;
-//     tr.appendChild(boton);
-//     // boton.textContent = "X";
-//     boton.setAttribute("id", "botonx");
-
-//     let tdNombre = document.createElement("td");
-//     tdNombre.textContent = cargarPersonajes();
-//     tr.appendChild(tdNombre);
-//     tdNombre.setAttribute("id", "nombre");
-//     tdNombre.setAttribute("class", "tdCreado");
-
-//     let tdDescripcion = document.createElement("td");
-//     tdDescripcion.textContent = formulario.descripcion;
-//     tr.appendChild(tdDescripcion);
-//     tdDescripcion.setAttribute("class", "tdCreado");
-
-//     let tdNumSerie = document.createElement("td");
-//     tdNumSerie.textContent = formulario.numeroSerie;
-//     tr.appendChild(tdNumSerie);
-//     tdNumSerie.setAttribute("class", "tdCreado");
-
-//     let tdEstado = document.createElement("td");
-//     tdEstado.textContent = formulario.estado;
-//     tr.appendChild(tdEstado);
-//     tdEstado.setAttribute("class", "tdCreado");
-
-//     let tdPrioridad = document.createElement("td");
-//     tdPrioridad.textContent = formulario.prioridad;;
-//     tr.appendChild(tdPrioridad);
-//     tdPrioridad.setAttribute("class", "tdCreado");
-
-//     cuerpoTabla.appendChild(tr);
-
-//     boton.onclick = function () {
-//         alert("Me voy a borrar")
-//         this.parentElement.remove();//elimino el padre de $boton
-//     };
-// });
 
 function filtro() {
 
@@ -243,80 +150,8 @@ function filtro() {
     }
 }
 
-function ModNombreX(id)
-{
-console.log(id);
-//const classID = document.getElementsById("TdModNombre");
-//alert("ID "+id);
-var valor_inicial = document.getElementById("TdModNombre_"+id).innerHTML;
-//document.getElementById("TdModNombre_"+id).innerHTML = '<input type="text" value="'+valor_inicial+'">';
-}/*
-for (var i =0; i<= classID.length; i++){
-    classID[i].addEventListener("dbclick",function(){
-        
-        alert("HOLA ID : ");   
-    });
-};
-*/
-
-// function update() {
-//     //coger sleector y traer
-//     const tds = document.querySelectorAll("tr");
-
-//     for (td of tds) {
-//         var manu = 0;
-//         console.log('td: ' + (manu+1))
-//         td.addEventListener("dblclick", function () {
-//             console.log('Clickkk dani');
-//             // crea un input con el valor de la celda
-//             let input = document.createElement('input');
-//             input.value = this.textContent;
-//             //se ejecuta cuando el input pierde el foco
-//             input.addEventListener("blur", function () {
-//                 removeInput(this);
-//             });
-
-//             // evento que se ejecuta cada vez que se deja de pulsar una tecla
-//             input.addEventListener("keydown", function (e) {
-//                 // la tecla 13, es el Enter
-//                 if (e.which == 13) {
-//                     removeInput(this);
-//                 }
-//             });
-//             // quito el contenido de la celda de la tabla
-//             this.textContent = "";
-//             // Pongo en la celda el input que hemos creado
-//             this.appendChild(input);
-//         });
-
-//     }
-// }
-
-// function removeInput(e) {
-//     e.parentElement.textContent = e.value;
-// }
-
 
 function update(row, data) {
-
-//     <form id="form">
-//     <div>NombreElemento: <input type="text" name="nombre" id="NombreElemento"></div>
-//     <div>Descripcion: <input type=text id="desElemento" name="descripcion"></div>
-//     <div>Num Serie: <input type=text id="numSerie" name="numero"></div>
-//     <div><legend><b>Estado: Activar/desactivar</b></legend>
-//         <input type="checkbox" id="chbox" value="Activo" name="estado"></div>
-//     <div> <legend><b>Prioridad</b></legend>
-//         <input type="radio" id="alta" name="prioridad" value="Alta">Alta
-//         <input type="radio" id="media" name="prioridad" value="Media">Media
-//         <input type="radio" id="baja" name="prioridad" value="Baja">Baja
-//     </div>
-//     <input type="submit" id="actualizar" value="actualizar"/>    
-// </form>
-// </div>
-
-
-    
-
     let NombreElemento = document.getElementById('NombreElemento');
     NombreElemento.value = data.nombre;
 
